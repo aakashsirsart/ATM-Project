32ad925569f34e0c206645be634d0de10dfd9453
aakashsirsart <103035915+aakashsirsart@users.noreply.github.com>
2022-07-18 10:55:12 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,12 @@
+package project1;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+
+}
